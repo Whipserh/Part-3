ccb18089afda4122f102d051dce4355855bd9a3d
Ryan McBride <mcbrider@shernet.sheridancollege.ca>
2024-03-27 10:09:45 -0400

    added moving camera to assignment

---
+++ Assets/Assignment/Script/Camera_controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Camera_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //move the camera using wsad
+        transform.Translate(Input.GetAxis("Horizontal")*speed*Time.deltaTime, Input.GetAxis("Vertical")*speed*Time.deltaTime, 0);
+    }
+}
